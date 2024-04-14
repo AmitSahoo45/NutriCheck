558140d39a6a1fc004378df347ffbb7c850521d1
miamitsahoo@gmail.com <miamitsahoo@gmail.com>
2024-04-14 17:17:53 +0530

    Update: Add image files

--- app/check/page.tsx
+++ app/check/page.tsx
@@ -3,19 +3,16 @@
 import React, { useEffect, useState } from 'react';
 import Webcam from 'react-webcam';
 import Tesseract from 'tesseract.js';
+import { Toast, Toaster, toast } from 'react-hot-toast'
+import Image from 'next/image';
 
 const CheckIndexPage: React.FC = () => {
     const [isWebcamOpen, setIsWebcamOpen] = useState(false);
     const [imageSource, setImageSource] = useState<string | null>(null);
     const [extractedText, setExtractedText] = useState<string | null>(null);
-    const [isMobile, setIsMobile] = useState(false);
+    const [uploadedImage, setUploadedImage] = useState<File | null>(null);
 
     const webcamRef = React.useRef<Webcam>(null);
-
-    useEffect(() => {
-        const isMobileDevice = /Mobi|Android/i.test(navigator.userAgent);
-        setIsMobile(isMobileDevice);
-    }, []);
 
     const toggleWebcam = () => {
         setIsWebcamOpen((prevState) => !prevState);
@@ -26,11 +23,35 @@ const CheckIndexPage: React.FC = () => {
         setImageSource(imageSrc || null);
 
         if (imageSrc) {
-            Tesseract.recognize(imageSrc, 'eng', {
-                logger: (m) => console.log(m),
-            }).then(({ data: { text } }) => {
-                setExtractedText(text);
-            });
+            Tesseract
+                .recognize(imageSrc, 'eng', {
+                    logger: (m) => console.log(m),
+                })
+                .then(({ data: { text } }) => {
+                    setExtractedText(text);
+                })
+                .catch((error) => {
+                    toast.error(error.message);
+                });
+        }
+    };
+
+    const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0] || null;
+        setUploadedImage(file);
+
+        if (file) {
+            const imageUrl = URL.createObjectURL(file);
+            Tesseract
+                .recognize(imageUrl, 'eng', {
+                    logger: (m) => console.log(m),
+                })
+                .then(({ data: { text } }) => {
+                    setExtractedText(text);
+                })
+                .catch((error) => {
+                    toast.error(error.message);
+                })
         }
     };
 
@@ -43,7 +64,7 @@ const CheckIndexPage: React.FC = () => {
                     videoConstraints={{
                         width: 640,
                         height: 480,
-                        facingMode: isMobile ? { exact: 'environment' } : 'user',
+                        facingMode: 'user',
                     }}
                 />
             )}
@@ -51,6 +72,14 @@ const CheckIndexPage: React.FC = () => {
             <button onClick={captureImage}>Capture Image</button>
             {imageSource && <img src={imageSource} alt="Captured" />}
             {extractedText && <p>Extracted Text: {extractedText}</p>}
+            <input type="file" onChange={handleImageUpload} />
+            {uploadedImage && <Image
+                src={URL.createObjectURL(uploadedImage)}
+                alt="Uploaded"
+                width={200}
+                height={360}
+            />}
+            <Toaster />
         </div>
     )
 }
